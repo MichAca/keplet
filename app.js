574423b3653a11d85d98ed3ada8c7ddca971a3fb
MichAca <m.aca@outlook.ph>
2019-09-11 10:44:37 +0800

    add compose and post functionality

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -17,15 +18,34 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-  res.render("home", {startingContent: homeStartingContent});
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts:posts
+  });
+
+});
+
+app.get("/about", function(req,res){
+  res.render("about", {aboutContent: aboutContent}); //js objects can be the same name as best practice
+});
+
+app.get("/contact", function(req,res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req,res){
+  res.render("compose");
 });
 
 
-
-
-
-
-
+app.post("/compose", function(req,res){
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+});
 
 
 app.listen(3000, function() {
